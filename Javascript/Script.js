96be4d76579eff32e69b9848bc318ac219408548
Rayat <rayat@Rayats-MacBook-Air.local>
2026-03-08 00:07:46 +0600

    JavaScript Basics

---
+++ Javascript/Script.js
@@ -0,0 +1,14 @@
+alert("Connected");
+console.log("Hello World");
+console.error("This is an error");
+console.warn("This is a warning");
+
+//let, const, var
+let age=30;
+age=31;
+console.log(age);
+
+ const name="John";
+ name="Doe"; // This will cause an error because name is a constant
+
+ 
