b26fbd8c2ba8be7bfd0c2ec2c9f08a7525600d15
Rayat <rayat@Rayats-MacBook-Air.local>
2026-03-08 00:59:48 +0600

    JavaScript Basics

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -10,5 +10,13 @@ console.log(age);
 
  const name="John";
  name="Doe"; // This will cause an error because name is a constant
-
+//String, number, boolean, null, undefined
+const name1 = "John";
+const age1 = 30;
+const rating = 4.5;
+const isStudent = true;
+const x = null;
+const y = undefined;
+//const z;
+console.log(typeof name1);
  
